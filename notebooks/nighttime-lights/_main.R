8107f2721e2c8d7a474f900f157d32f3b58cd6b9
Robert Andrew Marty <robmarty3@gmail.com>
2024-04-11 18:28:47 -0400

    init ntl

---
+++ notebooks/nighttime-lights/_main.R
@@ -0,0 +1,10 @@
+# Nighttime Lights: Main Script
+
+# Filepaths --------------------------------------------------------------------
+git_dir <- file.path("/Users", "rmarty", "Library", "CloudStorage", "OneDrive-WBG", "Documents",
+                     "GitHub", "niger-economic-monitoring", "notebooks", "nighttime-lights")
+
+# Packages ---------------------------------------------------------------------
+library(dplyr)
+library(sf)
+library(ggplot2)
